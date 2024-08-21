1e6c9983fb17bc92237e049eb469dbf1f123d2a8
EgorSivenko <egor.siv20@gmail.com>
2024-08-21 21:04:19 +0300

    Documented a proposed solution for abstracting the storage of the rate limiter's state

--- src/main/java/com/example/pragmasoft/exercise/ratelimit/RateLimitService.java
+++ src/main/java/com/example/pragmasoft/exercise/ratelimit/RateLimitService.java
@@ -16,6 +16,15 @@ public class RateLimitService {
     private static final int CAPACITY = 10;
     private static final long REFILL_PERIOD = 10000;
 
+    /**
+     * In this case, the thread-safe ConcurrentHashMap is used, but to improve the performance and scalability,
+     * we may use some centralized storage, e.g. Redis cache or a NoSQL database like Mongo.
+     * As for me, Redis would be better as it's a popular choice for distributed rate limiting
+     * due to its fast in-memory storage and support for distributed locks and data structures.
+     * In that case, we can store user-specific statistics, i.e. number of tokens and the time of the last refill,
+     * by accessing them with a client key or authorization token.
+     * The main operations used would be GET, SET, EXISTS, and EXPIRE.
+     */
     private final ConcurrentHashMap<String, TokenBucket> tokenBuckets = new ConcurrentHashMap<>();
     private final RequestNetworkClientKeyExtractor keyExtractor;
 
